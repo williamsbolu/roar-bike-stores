6773534e4cfdba40fc7e9a6cd44aff62dfc3e738
williams bolu <williams.bolu99gmail.com>
2023-09-02 22:53:17 +0100

    changed api route

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,5 @@
-export const ROARBIKES_API = 'http://127.0.0.1:3000';
+// export const ROARBIKES_API = 'http://127.0.0.1:3000';
+export const ROARBIKES_API = 'https://roar-bikes-store-api.onrender.com';
 
 export function sleep(time) {
     return new Promise((resolve, reject) => {
